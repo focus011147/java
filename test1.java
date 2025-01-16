1b0346652c72ba0fa4da15e0c2c3e60749acf8a9
focus011147 <147577267+focus011147@users.noreply.github.com>
2025-01-16 09:25:39 +0700

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+public class test1 {
+
+
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("+++++++++++++++++++++++++");
+        System.out.println("|Program calculate Grade|");
+        System.out.println("+++++++++++++++++++++++++");
+
+        System.out.println("input Data");
+        System.out.print("Enter score of subject #1 :");
+        int subject1 = scanner.nextInt();
+        System.out.print("Enter credit of subject #1 :");
+        int credit1 = scanner.nextInt();
+        System.out.print("Enter score of subject #2 :");
+        int subject2 = scanner.nextInt();
+        System.out.print("Enter credit of subject #2 :");
+        int credit2 = scanner.nextInt();
+        System.out.print("Enter score of subject #3 :");
+        int subject3 = scanner.nextInt();
+        System.out.print("Enter credit of subject #3 :");
+        int credit3 = scanner.nextInt();
+        System.out.print("Enter score of subject #4 :");
+        int subject4 = scanner.nextInt();
+        System.out.print("Enter credit of subject #4 :");
+        int credit4 = scanner.nextInt();
+        System.out.print("Enter score of subject #5 :");
+        int subject5 = scanner.nextInt();
+        
+
+
+
+
+        scanner.close();
+    }
+}
